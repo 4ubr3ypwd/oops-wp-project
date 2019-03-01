81bb640f5c027ce60453505659b639a0619e9c69
Aubrey Portwood <aubreypwd@gmail.com>
2019-03-01 12:36:28 -0700

    Base working plugin.

---
+++ oops-wp-project.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Plugin Name: oops-wp-project
+ * Description:
+ * Version:     1.0.0-dev
+ * Author:      Aubrey Portwood
+ * Author URI:  http://github.com/aubreypwd/oops-wp-project
+ * Text Domain: company-slug-project-slug
+ * Network:     False
+ * License:     GPLv2
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * @since       1.0.0
+ * @package     aubreypwd\oops-wp-project
+ *
+ * Built with:  oops-wp-project
+ */
+
+namespace aubreypwd\OopsWpProject;
+
+require_once 'vendor/autoload.php';
+
+/**
+ * Project function.
+ *
+ * @author Aubrey Portwood <code@aubreypwd.com>
+ * @since  1.0.0
+ *
+ * @return \Project Project object.
+ */
+function project() {
+	static $project = null;
+
+	if ( null === $project ) {
+
+		// Create the app and go!
+		$project = new Project( __FILE__ );
+
+		$project->register_hooks();
+	}
+
+	return $project;
+}
+
+// Wait until WordPress is ready, then go!
+add_action( 'plugins_loaded', 'aubreypwd\OopsWpProject\project' );
+
+// When we deactivate this plugin...
+register_deactivation_hook( __FILE__, array( project(), 'deactivate_plugin' ) );
---
+++ src/Project.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace aubreypwd\OopsWpProject;
+
+class Project implements \WebDevStudios\OopsWP\Utility\Hookable {
+	public function register_hooks() {
+	}
+
+	public function deactivate_plugin() {
+	}
+}
